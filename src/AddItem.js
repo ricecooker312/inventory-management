1756762e23b5322f0f61690a7c2ac9719f581060
guywh <guywh@Pavillion>
2024-06-05 19:26:56 -0500

    added a db.json file for json-like database using json-server, also added method to delete items

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,57 +1,95 @@
 import { useState } from "react";
 
 const AddItem = (props) => {
-    const [name, setName] = useState("")
-    const [price, setPrice] = useState(0)
-    const [type, setType] = useState("")
-    const [brand, setBrand] = useState("")
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [type, setType] = useState("");
+  const [brand, setBrand] = useState("");
 
-    const addItemButtonPressed = () => {
-        props.addItem({
-            "name": name,
-            "price": price,
-            "type": type,
-            "brand": brand
-        })
+  const addItemButtonPressed = () => {
+    props.addItem({
+      name: name,
+      price: price,
+      type: type,
+      brand: brand
+    });
 
-        setName("")
-        setPrice(0)
-        setType("")
-        setBrand("")
-    }
+    setName("");
+    setPrice(0);
+    setType("");
+    setBrand("");
+  };
 
-    return (
-        <div className="container">
-            <div className="row">
-                <h2>Add an Item</h2>
-            </div>
-            <div className="row">
-                <label htmlFor="name-field" className="mt-3">Name:</label>
-                <input className="form-control" id="name-field" type="text" value={name} onChange={(e) => {
-                    setName(e.target.value)
-                }} />
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Add an Item</h2>
+      </div>
+      <div className="row">
+        <label htmlFor="name-field" className="mt-3">
+          Name:
+        </label>
+        <input
+          className="form-control"
+          id="name-field"
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
 
-                <label htmlFor="price-field" className="mt-3">Price:</label>
-                <input className="form-control" id="price-field" type="number"value={price} onChange={(e) => {
-                    setPrice(e.target.value)
-                }} />
+        <label htmlFor="price-field" className="mt-3">
+          Price:
+        </label>
+        <input
+          className="form-control"
+          id="price-field"
+          type="number"
+          value={price}
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        />
 
-                <label htmlFor="type-field" className="mt-3">Type:</label>
-                <input className="form-control" id="type-field" type="text" value={type} onChange={(e) => {
-                    setType(e.target.value)
-                }} />
+        <label htmlFor="type-field" className="mt-3">
+          Type:
+        </label>
+        <input
+          className="form-control"
+          id="type-field"
+          type="text"
+          value={type}
+          onChange={(e) => {
+            setType(e.target.value);
+          }}
+        />
 
-                <label htmlFor="brand-field" className="mt-3">Brand:</label>
-                <input className="form-control" id="brand-field" type="text" value={brand} onChange={(e) => {
-                    setBrand(e.target.value)
-                }} />
-            </div>            
+        <label htmlFor="brand-field" className="mt-3">
+          Brand:
+        </label>
+        <input
+          className="form-control"
+          id="brand-field"
+          type="text"
+          value={brand}
+          onChange={(e) => {
+            setBrand(e.target.value);
+          }}
+        />
+      </div>
 
-            <div className="row mt-3">
-                <button type="button" className="btn btn-primary" onClick={addItemButtonPressed}>Add Item</button>
-            </div>    
-        </div>
-    )
-}
+      <div className="row mt-3">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={addItemButtonPressed}
+        >
+          Add Item
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default AddItem;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,62 +2,90 @@ import { useState } from "react";
 import React from "react";
 
 const SearchBar = (props) => {
-    const [name, setName] = useState("")
-    const [price, setPrice] = useState(0)
-    const [type, setType] = useState("")
-    const [brand, setBrand] = useState("")
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [type, setType] = useState("");
+  const [brand, setBrand] = useState("");
 
-    const searchButtonPressed = () => {
-        props.updateSearchParams({
-            "name": name,
-            "price": price,
-            "type": type,
-            "brand": brand
-        })
-    }
+  const searchButtonPressed = () => {
+    props.updateSearchParams({
+      name: name,
+      price: price,
+      type: type,
+      brand: brand,
+    });
+  };
 
-    return (
-        <div className="container">
-            <div className="row">
-                <h2>Search for an Item</h2>
-            </div>
-            <div className="row">
-
-                <div className="col mt-2">
-                    <label htmlFor="name-field">Name:</label>
-                    <input className="form-control" id="name-field" type="text" value={name} onChange={(e) => {
-                        setName(e.target.value)
-                    }} />
-                </div>
-
-                <div className="col mt-2">
-                    <label htmlFor="price-field">Max Price:</label>
-                    <input className="form-control" id="price-field" type="number"value={price} onChange={(e) => {
-                        setPrice(e.target.value)
-                    }} />
-                </div>
-
-                <div className="col mt-2">
-                    <label htmlFor="type-field">Type:</label>
-                    <input className="form-control" id="type-field" type="text" value={type} onChange={(e) => {
-                        setType(e.target.value)
-                    }} />
-                </div>
-
-                <div className="col mt-2">
-                    <label htmlFor="brand-field">Brand:</label>
-                    <input className="form-control" id="brand-field" type="text" value={brand} onChange={(e) => {
-                        setBrand(e.target.value)
-                    }} />
-                </div>
-
-            </div>
-            <div className="row mt-3">
-                <div className="col-4"></div>
-                <button type="button" className="btn btn-primary col-4" onClick={searchButtonPressed}>Search</button>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <h2>Search for an Item</h2>
+      </div>
+      <div className="row">
+        <div className="col mt-2">
+          <label htmlFor="name-field">Name:</label>
+          <input
+            className="form-control"
+            id="name-field"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
-    )
-}
+
+        <div className="col mt-2">
+          <label htmlFor="price-field">Max Price:</label>
+          <input
+            className="form-control"
+            id="price-field"
+            type="number"
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="col mt-2">
+          <label htmlFor="type-field">Type:</label>
+          <input
+            className="form-control"
+            id="type-field"
+            type="text"
+            value={type}
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="col mt-2">
+          <label htmlFor="brand-field">Brand:</label>
+          <input
+            className="form-control"
+            id="brand-field"
+            type="text"
+            value={brand}
+            onChange={(e) => {
+              setBrand(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-4"></div>
+        <button
+          type="button"
+          className="btn btn-primary col-4"
+          onClick={searchButtonPressed}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
